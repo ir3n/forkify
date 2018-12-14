82313ccb3198afd507fb12985a5084a102678057
ir3n <iren.borada@gmail.com>
2018-12-14 14:02:44 +0200

    liking functionality ready

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,11 @@
 import Search from "./models/Search";
 import Recipe from "./models/Recipe";
 import List from "./models/List";
+import Likes from "./models/Likes";
 import * as searchView from "./views/searchView";
 import * as recipeView from "./views/recipeView";
 import * as listView from "./views/listView";
+import * as likesView from "./views/likesView";
 
 import { elements, renderLoader, clearLoader } from "./views/base";
 
@@ -82,7 +84,7 @@ const controlRecipe = async () => {
       state.recipe.calcServings();
       //render recipe
       clearLoader();
-      recipeView.renderRecipe(state.recipe);
+      recipeView.renderRecipe(state.recipe, state.likes.isLiked(id));
     } catch (err) {
       alert("Error processing recipe!");
     }
@@ -118,6 +120,41 @@ elements.shopping.addEventListener("click", e => {
   }
 });
 
+/* =====LIKE CONTROLLER===== */
+//TESTING
+state.likes = new Likes();
+likesView.toggleLikeMenu(state.likes.getNumLikes());
+
+const controlLike = () => {
+  if (!state.likes) state.likes = new Likes();
+  const currentID = state.recipe.id;
+
+  //user has NOT yet liked current recipe
+  if (!state.likes.isLiked(currentID)) {
+    //add like to the state
+    const newLike = state.likes.addLike(
+      currentID,
+      state.recipe.title,
+      state.recipe.author,
+      state.recipe.img
+    );
+    //toggle the like button
+    likesView.toggleLikeBtn(true);
+    //add like to UI list
+    likesView.renderLike(newLike);
+
+    //user HAS liked current recipe
+  } else {
+    //remove like from the state
+    state.likes.deleteLike(currentID);
+    //toggle the like button
+    likesView.toggleLikeBtn(false);
+    //remove like from UI list
+    likesView.deleteLike(currentID);
+  }
+  likesView.toggleLikeMenu(state.likes.getNumLikes());
+};
+
 //handling recipe button clicks
 elements.recipe.addEventListener("click", e => {
   //if the target matches the dec-btn or any child of it
@@ -130,7 +167,11 @@ elements.recipe.addEventListener("click", e => {
     state.recipe.updateServings("inc");
     recipeView.updateServingsIngredients(state.recipe);
   } else if (e.target.matches(".recipe__btn--add, .recipe__btn--add *")) {
+    //add ingredients to shopping list
     controlList();
+  } else if (e.target.matches(".recipe__love, .recipe__love *")) {
+    //like controller
+    controlLike();
   }
 });
 window.l = new List();
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,26 +1,35 @@
 import { elements } from "./base";
-import { create } from "domain";
 
 export const getInput = () => elements.searchInput.value;
 
 export const clearInput = () => {
   elements.searchInput.value = "";
 };
+
 export const clearResults = () => {
   elements.searchResList.innerHTML = "";
   elements.searchResPages.innerHTML = "";
 };
+
 export const highlightSelected = id => {
-  if (document.querySelector(".results__link--active")) {
-    document
-      .querySelector(".results__link--active")
-      .classList.remove("results__link--active");
-  }
+  const resultsArr = Array.from(document.querySelectorAll(".results__link"));
+  resultsArr.forEach(el => {
+    el.classList.remove("results__link--active");
+  });
   document
-    .querySelector(`a[href="#${id}"]`)
+    .querySelector(`.results__link[href*="${id}"]`)
     .classList.add("results__link--active");
 };
-const limitRecipeTitle = (title, limit = 17) => {
+
+/*
+// 'Pasta with tomato and spinach'
+acc: 0 / acc + cur.length = 5 / newTitle = ['Pasta']
+acc: 5 / acc + cur.length = 9 / newTitle = ['Pasta', 'with']
+acc: 9 / acc + cur.length = 15 / newTitle = ['Pasta', 'with', 'tomato']
+acc: 15 / acc + cur.length = 18 / newTitle = ['Pasta', 'with', 'tomato']
+acc: 18 / acc + cur.length = 24 / newTitle = ['Pasta', 'with', 'tomato']
+*/
+export const limitRecipeTitle = (title, limit = 17) => {
   const newTitle = [];
   if (title.length > limit) {
     title.split(" ").reduce((acc, cur) => {
@@ -29,6 +38,8 @@ const limitRecipeTitle = (title, limit = 17) => {
       }
       return acc + cur.length;
     }, 0);
+
+    // return the result
     return `${newTitle.join(" ")} ...`;
   }
   return title;
@@ -37,15 +48,15 @@ const limitRecipeTitle = (title, limit = 17) => {
 const renderRecipe = recipe => {
   const markup = `
         <li>
-            <a class="likes__link" href="#${recipe.recipe_id}">
-                <figure class="likes__fig">
+            <a class="results__link" href="#${recipe.recipe_id}">
+                <figure class="results__fig">
                     <img src="${recipe.image_url}" alt="${recipe.title}">
                 </figure>
-                <div class="likes__data">
-                    <h4 class="likes__name">${limitRecipeTitle(
+                <div class="results__data">
+                    <h4 class="results__name">${limitRecipeTitle(
                       recipe.title
                     )}</h4>
-                    <p class="likes__author">${recipe.publisher}</p>
+                    <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
         </li>
@@ -53,42 +64,48 @@ const renderRecipe = recipe => {
   elements.searchResList.insertAdjacentHTML("beforeend", markup);
 };
 
+// type: 'prev' or 'next'
 const createButton = (page, type) => `
-  <button class="btn-inline results__btn--${type}" data-goto=${
+    <button class="btn-inline results__btn--${type}" data-goto=${
   type === "prev" ? page - 1 : page + 1
 }>
-    <span>Page ${type === "prev" ? page - 1 : page + 1}</span>
-    <svg class="search__icon">
-      <use href="img/icons.svg#icon-triangle-${
-        type === "prev" ? "left" : "right"
-      }"></use>
-    </svg>
-  </button>
+        <span>Page ${type === "prev" ? page - 1 : page + 1}</span>
+        <svg class="search__icon">
+            <use href="img/icons.svg#icon-triangle-${
+              type === "prev" ? "left" : "right"
+            }"></use>
+        </svg>
+    </button>
 `;
+
 const renderButtons = (page, numResults, resPerPage) => {
   const pages = Math.ceil(numResults / resPerPage);
+
   let button;
   if (page === 1 && pages > 1) {
-    //only button to go to next page
+    // Only button to go to next page
     button = createButton(page, "next");
   } else if (page < pages) {
-    //both buttons
+    // Both buttons
     button = `
-    ${createButton(page, "prev")}
-    ${createButton(page, "next")}
-    `;
+            ${createButton(page, "prev")}
+            ${createButton(page, "next")}
+        `;
   } else if (page === pages && pages > 1) {
-    //only button to go to prev page
+    // Only button to go to prev page
     button = createButton(page, "prev");
   }
+
   elements.searchResPages.insertAdjacentHTML("afterbegin", button);
 };
 
 export const renderResults = (recipes, page = 1, resPerPage = 10) => {
-  //render results of current page
+  // render results of currente page
   const start = (page - 1) * resPerPage;
   const end = page * resPerPage;
+
   recipes.slice(start, end).forEach(renderRecipe);
-  //render pagination buttons
+
+  // render pagination buttons
   renderButtons(page, recipes.length, resPerPage);
 };
